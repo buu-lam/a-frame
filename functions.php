1c18937629329d587d548b4a04b6586966774760
blle <840722+buu-lam@users.noreply.github.com>
2023-01-16 19:21:45 +0100

    fix php8 compatibility

--- functions.php
+++ functions.php
@@ -1,23 +1,33 @@
 <?php
 
-function useAfUnderscore() {
-    if (!file_exists('_')) {
-        function _($value) {
-            return \Af\_($value);
+namespace {
+
+    function useAfUnderscore() {
+        if (!file_exists('_')) {
+
+            function _($value) {
+                return \Af\_($value);
+            }
+
         }
     }
-}
-    
-namespace Af;
 
-function _($value) {
-    if (is_string($value)) {
-        return new Type\Str($value);
-    } else if (is_array($value)) {
-        return new Type\Arr($value);
-    } else if (is_numeric($value)) {
-        return new Type\Num($value);
-    } else {
-        return new Af\Type\Variable($value);
-    }
 }
+
+namespace Af {
+
+    function _($value) {
+        if (is_string($value)) {
+            return new Type\Str($value);
+        } else if (is_array($value)) {
+            return new Type\Arr($value);
+        } else if (is_numeric($value)) {
+            return new Type\Num($value);
+        } else {
+            return new Af\Type\Variable($value);
+        }
+    }
+
+}
+
+
