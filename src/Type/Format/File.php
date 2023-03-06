9e88f70760e15162dc3263aa8a99023ae6060b6a
blle <840722+buu-lam@users.noreply.github.com>
2023-03-07 00:07:42 +0100

    str: fix file functions and add

--- src/Type/Format/File.php
+++ src/Type/Format/File.php
@@ -16,12 +16,12 @@ trait File {
         );
     }
     
-    public function putContent($content) : string {
+    public function putContent($content) {
         file_put_contents($this->value, $content);
         return $this;
     }
     
-    public function appendContent($content) : string {
+    public function appendContent($content) {
         file_put_contents($this->value, $content, FILE_APPEND);
         return $this;
     }
@@ -37,6 +37,16 @@ trait File {
     
     public function appendToFile($path) {
         file_put_contents($path, $this->value, FILE_APPEND);
+        return $this;
+    }
+    
+    public function chmod(int $permissions) {
+        chmod($this->value, $permissions);
+        return $this;
+    }
+    
+    public function chown($user) {
+        chown($this->value, $user);
         return $this;
     }
 }
