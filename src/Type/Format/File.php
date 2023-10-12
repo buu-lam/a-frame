1eb081a7e9b85b02c49094d64b5e0d829fd83985
blle <840722+buu-lam@users.noreply.github.com>
2023-10-12 23:49:55 +0200

    file utils

--- src/Type/Format/File.php
+++ src/Type/Format/File.php
@@ -10,6 +10,10 @@ trait File {
         return file_exists($this->value);
     }
     
+    public function isADirectory(): bool {
+        return is_dir($this->value);
+    }
+    
     public function getContent() : Str {
         return $this->cloned(
             file_get_contents($this->value)
@@ -49,4 +53,29 @@ trait File {
         chown($this->value, $user);
         return $this;
     }
+    
+    public function dirname(int $levels = 1) {
+        return $this->cloned(
+            dirname($this->value, $levels)
+        );
+    }
+    
+    public function basename(string $suffix = '') {
+        return $this->cloned(
+            basename($this->value, $suffix)
+        );
+    }
+    
+    public function mkdir(int $permissions = 0777, bool $recursive = false, /*?resource*/ $context = null) {
+        mkdir($this->value, $permissions, $recursive, $context);
+        return $this;
+    }
+    
+    
+    public function copyTo($to, /*?resource*/ $context = null) {
+        copy($this->value, "$to", $context);
+        $this;
+    }
+    
+    
 }
