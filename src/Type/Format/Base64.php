add4e1da56e72a11b63ff9d493348ef6e3d33943
blle <840722+buu-lam@users.noreply.github.com>
2024-01-25 23:41:21 +0100

    format

--- src/Type/Format/Base64.php
+++ src/Type/Format/Base64.php
@@ -3,12 +3,13 @@
 namespace Af\Type\Format;
 
 trait Base64 {
+
     public function base64Encode() {
         return $this->cloned(
                 base64_encode($this->value)
         );
     }
-    
+
     public function base64Decode() {
         return $this->cloned(
                 base64_decode($this->value)
